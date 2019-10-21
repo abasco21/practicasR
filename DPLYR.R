ec8c164362294f13ab94bf4e95d73b18ab2fbe96
Alejandro <alejandrobasco@gmail.com>
2019-10-21 15:43:04 +0200

    subiendo DPLY inicio

---
+++ DPLYR.R
@@ -0,0 +1,9 @@
+install.packages("nycflights13")
+library("nycflights13")
+library("tidyr")
+View(flights)
+years <- select(flights, year)
+mad <- filter(flights, origin == "LGA")
+year2 <- mutate(flights, year2 = flights$year + 1)
+arrange(flights, year)
+
